d515613abce0bf538350b740ab2a31c97dcfc131
mohamed bougra <mohamed.bougra@edu.uca.ma>
2023-05-06 19:21:21 +0100

    Create SIDE bar and NAV bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./client-login/login.component";
+import { ClientComponent } from "./client/client.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "login", component: LoginComponent },
+  { path: "clientHome", component: ClientComponent },
 ];
 
 @NgModule({
--- src/app/client-nav-menu/nav-menu.component.ts
+++ src/app/client-nav-menu/nav-menu.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-nav-menu',
-  templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  selector: "app-nav-menu",
+  templateUrl: "./nav-menu.component.html",
+  styleUrls: ["./nav-menu.component.css"],
 })
 export class NavMenuComponent implements OnInit {
+  doesHaveaccount: Boolean = false;
 
-  constructor() { }
+  isLoggedIn: Boolean = false;
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  numTel = window.sessionStorage.getItem("username");
+
+  ngOnInit(): void {}
+
+  onLogout() {
+    this.router.navigate(["/"]);
+    window.location.reload();
   }
-
 }
