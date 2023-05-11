c3b586d3333c9dfcb5646ddd61f6f188f8ba26ad
halima <halima>
2023-05-11 23:08:53 +0200

    Routing + add content

--- src/app/client-recapitul-paiement/recapitul-paiement.component.ts
+++ src/app/client-recapitul-paiement/recapitul-paiement.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class RecapitulPaiementComponent implements OnInit {
   title = 'Paiment de facture';
   title1='Paiement par reference';
-  title2='Selectioner pour payer'
+  title2='Recapitulatif des informations saisies'
   image='../assets/images/Logo_inwi.png';
   imageTitle= 'PAIMENT DE FACTURES  RECHARGES';
   Creancier="Creancier";
@@ -18,6 +18,11 @@ export class RecapitulPaiementComponent implements OnInit {
   soutitle2="SIDACTION";
   soutitle3="ANAS";
   value='10.00 DH';
+
+ reference: string ="10000119373";
+
+ description: string ="Don Sidaction";
+ prixTTC: number=10.00; 
 
 
 
